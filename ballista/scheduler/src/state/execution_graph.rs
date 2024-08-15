a2d805735a81750d61f05fa41ff7d208a7858289
张林伟 <lewiszlw520@gmail.com>
2024-08-15 16:49:15 +0800

    Correctly set job end_time

--- ballista/scheduler/src/state/execution_graph.rs
+++ ballista/scheduler/src/state/execution_graph.rs
@@ -596,6 +596,7 @@ impl ExecutionGraph {
 
     /// fail job with error message
     pub fn fail_job(&mut self, error: String) {
+        self.end_time = timestamp_millis();
         self.status = JobStatus {
             job_id: self.job_id.clone(),
             status: Some(Status::Failed(FailedJob {
@@ -622,6 +623,7 @@ impl ExecutionGraph {
             .map(|l| l.try_into())
             .collect::<Result<Vec<_>>>()?;
 
+        self.end_time = timestamp_millis();
         self.status = JobStatus {
             job_id: self.job_id.clone(),
             status: Some(job_status::Status::Successful(SuccessfulJob {
@@ -632,10 +634,6 @@ impl ExecutionGraph {
                 ended_at: self.end_time,
             })),
         };
-        self.end_time = SystemTime::now()
-            .duration_since(UNIX_EPOCH)
-            .unwrap()
-            .as_millis() as u64;
 
         Ok(())
     }
